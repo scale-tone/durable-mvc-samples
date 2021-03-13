6d039ad607ab0007f6d7b564787d7a136f96e59e
Konstantin Lepeshenkov <konstantin.lepeshenkov@gmail.com>
2021-03-13 21:05:54 +0100

    Initial commit

---
+++ CounterEntity/index.ts
@@ -0,0 +1,39 @@
+import * as DurableFunctions from "durable-functions"
+
+import { DurableEntity, VisibilityEnum } from '../Common/DurableEntity'
+import { CounterState } from '../ui/src/shared/CounterState';
+
+// Sample counter entity
+class CounterEntity extends DurableEntity<CounterState>
+{
+    async add(value: number) {
+        this.state.history.unshift(this.state.countContainer.count);
+        this.state.history.splice(10, 1);
+
+        this.state.countContainer.count += value;
+    }
+
+    async substract(value: number) {
+        this.state.history.unshift(this.state.countContainer.count);
+        this.state.history.splice(10, 1);
+
+        this.state.countContainer.count -= value;
+    }
+
+    // Overriding visibility
+    protected get visibility(): VisibilityEnum { return VisibilityEnum.ToEveryone; }
+
+    // Custom state initialization for a newly created entity
+    protected initializeState(): CounterState {
+
+        var newState = new CounterState();
+
+        newState.countContainer.count = 1;
+        newState.title = `Counter-${new Date().toISOString()}`;
+
+        return newState;
+    }
+}
+
+// Boilerplate to expose CounterEntity as a Durable Entity
+export default DurableFunctions.entity((ctx) => new CounterEntity(ctx).handleSignal());
---
+++ manage-entities/index.ts
@@ -0,0 +1,83 @@
+import { Context, HttpRequest } from "@azure/functions"
+import * as DurableFunctions from 'durable-functions'
+
+import { SignalArgumentContainer } from '../common/SignalArgumentContainer';
+import { DurableEntityStateContainer } from '../common/DurableEntityStateContainer';
+import { DurableEntityClientStateContainer } from '../ui/src/shared/common/DurableEntityClientStateContainer';
+import { ClientPrincipalHeaderName } from '../ui/src/shared/common/Constants';
+
+// Handles basic operations 
+export default async function (context: Context, req: HttpRequest): Promise<void> {
+
+    const entityName = context.bindingData.entityName.toString();
+    const entityKey = context.bindingData.entityKey?.toString();
+    const signalName = context.bindingData.signalName?.toString();
+    const callingUser = req.headers[ClientPrincipalHeaderName];
+
+    const durableClient = DurableFunctions.getClient(context);
+
+    if (req.method === "POST") {
+        
+        await durableClient.signalEntity(new DurableFunctions.EntityId(entityName, entityKey),
+            signalName,
+            <SignalArgumentContainer>{ argument: req.body, __metadata: { callingUser } }
+        );
+
+    } else if (!entityKey) {
+
+        const entityNameString = `@${entityName}@`;
+        const statuses = (await durableClient.getStatusAll())
+            // Filtering out entities
+            .filter(s => (s.input as any)?.exists === true && s.instanceId.startsWith(entityNameString))
+            .map(s => {
+
+                var stateContainer = (s.input as any)?.state;
+
+                // For some reason, state comes in form of a string here - so need to convert
+                if (typeof (stateContainer) === 'string') {
+                    stateContainer = JSON.parse(stateContainer);
+                }
+
+                return { instanceId: s.instanceId, stateContainer: stateContainer as DurableEntityStateContainer<any> };
+            })
+
+            // Checking access rights
+            .filter(s => DurableEntityStateContainer.isAccessAllowed(s.stateContainer, callingUser))
+
+            // Converting to ClientStateContainer
+            .map(s => <DurableEntityClientStateContainer> {
+                entityKey: s.instanceId.substr(entityNameString.length),
+                version: s.stateContainer.__metadata.version,
+                state: s.stateContainer.state
+            });
+
+        context.res = { body: statuses };
+
+    } else {
+
+        const stateResponse = await durableClient.readEntityState(new DurableFunctions.EntityId(entityName, entityKey));
+
+        if (!stateResponse || !stateResponse.entityExists) {
+            
+            context.res = { status: 404 };
+
+        } else {
+
+            const stateContainer = stateResponse.entityState as DurableEntityStateContainer<any>;
+
+            if (DurableEntityStateContainer.isAccessAllowed(stateContainer, callingUser)) {
+                
+                context.res = {
+                    body: <DurableEntityClientStateContainer>{
+                        version: stateContainer.__metadata.version,
+                        state: stateContainer.state
+                    }
+                };
+
+            } else {
+
+                context.res = { status: 403 };
+            }
+        }
+    }
+};

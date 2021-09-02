85207c64388324b082ad439bc641531a0621772d
Konstantin Lepeshenkov <5447190+scale-tone@users.noreply.github.com>
2021-09-02 19:08:28 +0200

    migrated to latest version of durable-functions

--- universal-tracker/TrackerEntity/index.ts
+++ universal-tracker/TrackerEntity/index.ts
@@ -1,5 +1,5 @@
 
 import * as DurableFunctions from "durable-functions"
 import { TrackerEntity } from '../DurableEntities/TrackerEntity';
-export default DurableFunctions.entity((ctx) => new TrackerEntity(ctx).handleSignal());
+export default DurableFunctions.entity((ctx) => new TrackerEntity(ctx as any).handleSignal());
         

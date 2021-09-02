8a92619af2153c18bcb0b67a7defe60fecb86edc
Konstantin Lepeshenkov <5447190+scale-tone@users.noreply.github.com>
2021-09-02 18:41:10 +0200

    migrated to latest version of durable-functions

--- appointments-sample/AppointmentEntity/index.ts
+++ appointments-sample/AppointmentEntity/index.ts
@@ -1,5 +1,5 @@
 
 import * as DurableFunctions from "durable-functions"
 import { AppointmentEntity } from '../DurableEntities/AppointmentEntity';
-export default DurableFunctions.entity((ctx) => new AppointmentEntity(ctx).handleSignal());
+export default DurableFunctions.entity((ctx) => new AppointmentEntity(ctx as any).handleSignal());
         
